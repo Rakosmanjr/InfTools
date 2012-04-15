06a969f743ea4eb59069f8a2380b85625761d504
James Rakos <sterben.erdlinge@gmail.com>
2012-04-15 13:28:08 -0700

    file additions

---
+++ InfTools/src/com/rakosmanjr/InfTools/InfTools.java
@@ -0,0 +1,78 @@
+package com.rakosmanjr.InfTools;
+
+import java.util.*;
+import java.util.logging.Logger;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class InfTools extends JavaPlugin
+{
+	Logger log;
+	public Map<String, Boolean> isInfTool;
+	PlayerInteractListener pil;
+	FileConfiguration config;
+	
+	public void onEnable ()
+	{	
+		log = this.getLogger();
+		log.info("InfTools has been enabled.");
+		
+		this.isInfTool = new HashMap<String, Boolean>();
+		this.pil = new PlayerInteractListener(this);
+		
+		config = this.getConfig();
+		
+		for (String p : config.getKeys(false))
+		{
+			isInfTool.put(p, config.getBoolean(p));
+		}
+		
+		this.getServer().getPluginManager().registerEvents(pil, this);
+	}
+	
+	public void onDisable ()
+	{
+		for (String p : isInfTool.keySet())
+		{
+			config.set(p, isInfTool.get(p));
+		}
+		
+		this.saveConfig();
+		
+		log.info("InfTools has been disabled.");
+	}
+	
+	public boolean onCommand (CommandSender sender, Command cmd, String commandLabel, String[] args)
+	{
+		Player player = null;
+		
+		if (sender instanceof Player)
+		{
+			player = (Player)sender;
+		}
+		
+		//command is /inftool
+		if (player != null && player.hasPermission("inftools.inftool"))
+		{
+			if (cmd.getName().equalsIgnoreCase("inftool"))
+			{
+				if (args.length == 0)
+				{
+					this.isInfTool.put(player.getName(), this.isInfTool.containsKey(player.getName()) ? !this.isInfTool.get(player.getName()) : true);
+				}
+				else
+				{
+					sender.sendMessage(ChatColor.RED
+							+ "Incorrect number of arguments!");
+				}
+			}
+		}
+		
+		return false;
+	}
+}
---
+++ InfTools/src/com/rakosmanjr/InfTools/PlayerInteractListener.java
@@ -0,0 +1,81 @@
+package com.rakosmanjr.InfTools;
+
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.ItemStack;
+
+public class PlayerInteractListener implements Listener
+{
+	int[] allowedIDs;
+	InfTools plugin;
+	
+	public PlayerInteractListener (InfTools plugin)
+	{
+		this.plugin = plugin;
+		
+		allowedIDs = new int[26];
+		allowedIDs[0] = 267;//iron sword
+		allowedIDs[1] = 255;//iron shovel
+		allowedIDs[2] = 257;//iron pick
+		allowedIDs[3] = 258;//iron axe
+		allowedIDs[4] = 292;//iron hoe
+		allowedIDs[5] = 268;//wood sword
+		allowedIDs[6] = 269;//wood shovel
+		allowedIDs[7] = 270;//wood pick
+		allowedIDs[8] = 271;//wood axe
+		allowedIDs[9] = 290;//wood hoe
+		allowedIDs[10] = 272;//stone sword
+		allowedIDs[11] = 273;//stone shovel
+		allowedIDs[12] = 274;//stone pick
+		allowedIDs[13] = 275;//stone axe
+		allowedIDs[14] = 291;//stone hoe
+		allowedIDs[15] = 283;//gold sword
+		allowedIDs[16] = 284;//gold shovel
+		allowedIDs[17] = 285;//gold pick
+		allowedIDs[18] = 286;//gold axe
+		allowedIDs[19] = 294;//gold hoe
+		allowedIDs[20] = 276;//diamond sword
+		allowedIDs[21] = 277;//diamond shovel
+		allowedIDs[22] = 278;//diamond pick
+		allowedIDs[23] = 279;//diamond axe
+		allowedIDs[24] = 293;//diamond hoe
+		allowedIDs[25] = 346;//fishing pole
+	}
+	
+	@EventHandler
+	public void onPlayerInteract (PlayerInteractEvent event)
+	{
+		if (!event.equals(null))
+		{
+			Player player = event.getPlayer();
+			ItemStack itemStack = event.getItem();
+			Material mat = itemStack.getType();
+			if (mat.isBlock() || mat.isEdible() || mat.isRecord())
+			{
+				return;
+			}
+			if (plugin.isInfTool.containsKey(player.getName()) ? plugin.isInfTool
+					.get(player.getName()) : false)
+			{
+				
+				for (int i = 0; i < allowedIDs.length; i++)
+				{
+					if (mat.getId() == allowedIDs[i])
+					{
+						itemStack.setDurability((short)0);
+						
+						if (player.getItemInHand().getTypeId() == mat.getId())
+						{
+							player.setItemInHand(itemStack);
+						}
+						
+						break;
+					}
+				}
+			}
+		}
+	}
+}
